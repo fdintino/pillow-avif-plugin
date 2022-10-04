9bd3198137c0e6b51165a21f17425b7213f4da32
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 00:15:02 +0000

    Adding numeric_owner as keyword arguement

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -316,7 +316,7 @@ def extract_dep(url, filename):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tgz, sources_dir)
